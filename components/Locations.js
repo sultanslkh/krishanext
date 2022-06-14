e4acded87206fe28ee47cf5e27b08e616dc14c28
sultan.salikh <sultansalikh07@gmail.com>
2022-06-14 19:00:53 +0600

    change 14.06.22[1]

--- components/Locations.js
+++ components/Locations.js
@@ -1,4 +1,4 @@
-import { React, useRef, useEffect } from 'react';
+import { React } from 'react';
 import Link from 'next/link';
 import stylish from '../styles/Locations.module.css';
 
@@ -6,28 +6,39 @@ function Locations(props) {
 	return (
 		<div>
 			<div className={stylish.main_drop_menu}>
+				<div className={stylish.triangle} />
 				<span onClick={props.hideModal}>&#88;</span>
 				<div className={stylish.main_drop_inner}>
 					<div className={stylish.index_region}>
-						<input placeholder="Поиск по городу, району, микрорайону" />
+						<div className={stylish.input_body}>
+							<div className={stylish.input_actions}>
+								<button className={stylish.btn_search}>kek</button>
+							</div>
+							<input placeholder="Поиск по городу, району, микрорайону" />
+						</div>
 					</div>
 					<div className={stylish.drop_selects}>
-						<select className={stylish.select} size="20">
-							{props.locations.map((loc) => (
-								<option>{loc.title}</option>
-							))}
-						</select>
+						<div className={stylish.leveled_select_visiable}>
+							<select className={stylish.select} size="20">
+								{props.locations.map((loc) => (
+									<option>{loc.title}</option>
+								))}
+							</select>
+							<Link href="/">
+								<a className={stylish.btn_primary_region}>Выбрать</a>
+							</Link>
+						</div>
+						<div className={stylish.leveled_select_visiable2}>
+							<select className={stylish.select} size="20">
+								{props.locations.map((loc) => (
+									<option>{loc.title}</option>
+								))}
+							</select>
+							<Link href="/">
+								<a className={stylish.btn_primary_region}>Выбрать</a>
+							</Link>
+						</div>
 					</div>
-					<div className={stylish.drop_selects_visible}>
-						<select className={stylish.select} size="20">
-							{props.locations.map((loc) => (
-								<option>{loc.title}</option>
-							))}
-						</select>
-					</div>
-					<Link href="/">
-						<a className={stylish.btn_primary_region}>Выбрать</a>
-					</Link>
 				</div>
 			</div>
 
